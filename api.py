10218fa958a29ceb43cf87b0371e410f68d62820
proger247 <comelkov2012@gmail.com>
2020-10-01 22:10:44 +0300

    update

--- api.py
+++ api.py
@@ -1,13 +1,15 @@
-import email
+import json
+
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 import requests
 
 
 class PetFriends:
+
     def __init__(self):
         self.base_url = 'https://petfriends1.herokuapp.com/'
 
-    def get_api_key(self, email: str, password: str):
+    def get_api_key(self, email: str, password: str) -> json:
         """This method allows to get API key which should be used for other API methods."""
 
         headers = {
@@ -16,7 +18,6 @@ class PetFriends:
         }
         res = requests.get(self.base_url + 'api/key', headers=headers)
         status = res.status_code
-
         result = ""
         try:
             result = res.json()
@@ -25,7 +26,7 @@ class PetFriends:
 
         return status, result
 
-    def get_list_of_pets(self, auth_key, filter):
+    def get_list_of_pets(self, auth_key: json, filter: str) -> json:
         """This method allows to get the list of pets."""
 
         headers = {'auth_key': auth_key['key']}
@@ -42,7 +43,7 @@ class PetFriends:
 
         return status, result
 
-    def add_information_about_new_pet(self, auth_key, name, animal_type, age, pet_photo):
+    def add_information_about_new_pet(self, auth_key: json, name: str, animal_type: str, age: int, pet_photo: str) -> json:
         """This method allows to add information about new pet."""
 
         data = MultipartEncoder(
@@ -65,7 +66,7 @@ class PetFriends:
 
         return status, result
 
-    def add_information_about_new_pet_without_photo(self, auth_key, name, animal_type, age):
+    def add_information_about_new_pet_without_photo(self, auth_key: json, name: str, animal_type: str, age: int) -> json:
         """This method allows to add information about new pet without photo."""
 
         data = MultipartEncoder(
@@ -87,7 +88,7 @@ class PetFriends:
 
         return status, result
 
-    def add_photo_of_pet(self,auth_key, pet_id, pet_photo):
+    def add_photo_of_pet(self,auth_key: json, pet_id: str, pet_photo: str) -> json:
         """This method allows to add photo of a pet."""
 
         data = MultipartEncoder(
@@ -107,7 +108,7 @@ class PetFriends:
 
         return status, result
 
-    def update_information_about_pet(self, auth_key, pet_id, name, animal_type, age):
+    def update_information_about_pet(self, auth_key: json, pet_id: str, name: str, animal_type: str, age: int) -> json:
         """This method allows to update information about pet."""
 
         data = {
@@ -128,12 +129,12 @@ class PetFriends:
 
         return status, result
 
-    def delete_pet_from_database(self,auth_key, pet_id):
+    def delete_pet_from_database(self, auth_key: json, pet_id: str) -> json:
         """This method allows to delete information about pet from database."""
 
         headers = {'auth_key': auth_key['key']}
 
-        res = requests.get(self.base_url + f'api/pets/{pet_id}', headers=headers, params=filter)
+        res = requests.delete(self.base_url + f'api/pets/{pet_id}', headers=headers)
         status = res.status_code
 
         result = ""
--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
 valid_email = 'com2012@gmail.com'
 valid_password = 'A9D8D5D7'
 valid_photo = "images/parrot1.jpg"
+
+#negative test-cases data
+
+# api_key_in_symbols = 'привет'
